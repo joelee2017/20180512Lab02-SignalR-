c09e47344a8f78c93ebbd134a81ed3727aab68e0
joe lee <as955133@gmail.com>
2018-05-12 23:09:22 +0800

    第二次練習，後面由輝哥接手完成send功能

---
+++ 20180512Lab02 從無到有的建立SignalR/MyHub1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace _20180512Lab02_從無到有的建立SignalR
+{
+    [HubName("IM")]
+    public class MyHub1 : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello("登入成功!!");
+        }
+
+        public void Send(string msg)
+        {
+            Clients.All.receive("get msg: " + msg);
+        }
+    }
+}
---
+++ 20180512Lab02 從無到有的建立SignalR/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_20180512Lab02_從無到有的建立SignalR.Startup1))]
+
+namespace _20180512Lab02_從無到有的建立SignalR
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // 如需如何設定應用程式的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
